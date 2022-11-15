30e8cb5a724ae0aaa33f1da5b890d37c4b11afcc
Kawson <piegusek20@gmail.com>
2022-11-15 22:18:29 +0100

    Changed main functions names.

--- Lab3_Cars/src/main/java/com/architekturausluginternetowych/lab2_/CarsApp.java
+++ Lab3_Cars/src/main/java/com/architekturausluginternetowych/lab2_/CarsApp.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Lab2Application {
-    
+public class CarsApp {
+
     public static void main(String[] args) {
-        SpringApplication.run(Lab2Application.class, args);
+        SpringApplication.run(CarsApp.class, args);
     }
 
 }
