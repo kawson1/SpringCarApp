027563fb53d4d01311651f1e2075b447712cbf98
Kawson <piegusek20@gmail.com>
2022-11-17 00:38:16 +0100

    Added listing all models, adding and deleting features.

--- WebView/js/dom_utils.js
+++ WebView/js/dom_utils.js
@@ -8,3 +8,25 @@ export function clearElementChildren(element) {
         element.removeChild(element.firstChild);
     }
 }
+
+/**
+ * Creates cell in HTML table - return td element
+ * @param text - text which will be in cell
+ * @returns {HTMLTableCellElement} - td element
+ */
+export function createTextCell(text) {
+    const td = document.createElement('td');
+    td.appendChild(document.createTextNode(text));
+    return td;
+}
+
+export function createButtonCell(text, action) {
+    const td = document.createElement('td');
+    const button = document.createElement('button');
+    button.appendChild(document.createTextNode(text));
+    button.addEventListener('click', action);
+    td.appendChild(button);
+    return td;
+}
+
+
--- WebView/model_list/model_list.js
+++ WebView/model_list/model_list.js
@@ -1,5 +1,12 @@
 import {getBackendUrl} from "../js/configuration.js";
-import {clearElementChildren} from "../js/dom_utils.js"
+import {clearElementChildren, createTextCell, createButtonCell} from "../js/dom_utils.js"
+
+window.addEventListener('load', () => {
+    const modelForm = document.getElementById('modelForm');
+    modelForm.addEventListener('submit', event => createModel(event));
+
+    fetchAndDisplayModels();
+})
 
 /**
  * Gets all models and then modify DOM tree to display them.
@@ -8,8 +15,8 @@ function fetchAndDisplayModels() {
     const xhttp = new XMLHttpRequest();
 
     xhttp.onreadystatechange = function () {
-        if (this.readyState == 4 && this.status == 200) {
-            let object = JSON.parse(this.responseText);
+        if (this.readyState === 4 && this.status === 200) {
+            displayModels(JSON.parse(this.responseText));
         }
     };
 
@@ -25,13 +32,52 @@ function fetchAndDisplayModels() {
 function displayModels(models) {
     let tableBody = document.getElementById('tableBody');
     clearElementChildren(tableBody);
-    models.forEach(model => {
+    models.models.forEach(model => {
         tableBody.appendChild(createTableRow(model));
     })
 }
 
+/**
+ * Creates row in table - returns tr element
+ * @param model {string} - name of model
+ */
 //TODO: create function which will create fields in row table.
 function createTableRow(model) {
-     let row = document.createElement('tr');
-     tr.appendChild();
+    let tr = document.createElement('tr');
+    tr.appendChild(createTextCell(model));
+    tr.appendChild(createButtonCell('delete', () => deleteModel(model)));
+    return tr;
+}
+
+function deleteModel(model){
+    const xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState === 4 && this.status === 202){
+            fetchAndDisplayModels();
+        }
+    };
+
+    xhttp.open("DELETE", getBackendUrl() + '/api/models/' + model, true);
+    xhttp.send();
+}
+
+function createModel(event) {
+    event.preventDefault();
+
+    const xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState === 4 && this.status === 201){
+            fetchAndDisplayModels();
+        }
+    };
+
+    xhttp.open("POST", getBackendUrl() + '/api/models/', true);
+
+    const request = {
+        'name': document.getElementById('modelName').value,
+        'hp': document.getElementById('horsePower').value
+    };
+
+    xhttp.setRequestHeader('Content-Type', 'application/json');
+    xhttp.send(JSON.stringify(request));
 }
