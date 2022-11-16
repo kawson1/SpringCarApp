0f58a8c0ceec598e1412fd2abe8953d0a51b9abf
Kawson <piegusek20@gmail.com>
2022-11-16 02:13:47 +0100

    Added some js files (incomplete showing models list)

--- WebView/model_list/model_list.js
+++ WebView/model_list/model_list.js
@@ -1,10 +1,37 @@
-let xhttp = new XMLHttpRequest();
+import {getBackendUrl} from "../js/configuration.js";
+import {clearElementChildren} from "../js/dom_utils.js"
 
-xhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-        let object = JSON.parse(this.responseText);
-    }
-};
+/**
+ * Gets all models and then modify DOM tree to display them.
+ */
+function fetchAndDisplayModels() {
+    const xhttp = new XMLHttpRequest();
 
-xhttp.open("GET", "http://localhost:8080/api/models", async=true);
-xhttp.send();
+    xhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            let object = JSON.parse(this.responseText);
+        }
+    };
+
+    xhttp.open("GET", getBackendUrl() + "/api/models", true);
+    xhttp.send();
+}
+
+/**
+ * Updates the DOM tree to display models
+ *
+ * @param ({models: string[]} all available models
+ */
+function displayModels(models) {
+    let tableBody = document.getElementById('tableBody');
+    clearElementChildren(tableBody);
+    models.forEach(model => {
+        tableBody.appendChild(createTableRow(model));
+    })
+}
+
+//TODO: create function which will create fields in row table.
+function createTableRow(model) {
+     let row = document.createElement('tr');
+     tr.appendChild();
+}
