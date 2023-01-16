dfb2539b4b080029e9bc0f3b80e637473e340cca
Kawson <piegusek20@gmail.com>
2023-01-16 23:41:13 +0100

    .

--- M2_server/WebView/car_view/car_view.js
+++ M2_server/WebView/car_view/car_view.js
@@ -2,6 +2,12 @@ import {getBackendUrl} from "../js/configuration.js";
 import {clearElementChildren, createButtonCell, createTextCell} from "../js/dom_utils.js";
 
 window.addEventListener('load', ()=>{
+    const carImageForm = document.getElementById('carImageForm');
+    carImageForm.addEventListener('submit', event=>uploadCarImageAction(event));
+
+    const carImageDownload = document.getElementById('carImageDownload');
+    carImageDownload.addEventListener('click', event=>downloadCarImageAction(event));
+
     fetchAndDisplayCarDetails();
 })
 
@@ -24,4 +30,30 @@ function displayCarDetails(car){
     document.getElementById('vin').innerText += car.vin;
     document.getElementById('color').innerText += car.color;
     document.getElementById('model').innerText += urlParams.get('model');
+}
+
+function uploadCarImageAction(event){
+    event.preventDefault();
+
+    const xhttp = new XMLHttpRequest();
+    const urlParams = new URLSearchParams(window.location.search);
+
+    xhttp.open("PUT", getBackendUrl() + '/api/cars/' + urlParams.get('car') + '/upload', true);
+
+    let request = new FormData();
+    request.append("image", document.getElementById('car_image').files[0]);
+    request.append("author", document.getElementById('author').value);
+    request.append("description", document.getElementById('description').value);
+
+    xhttp.send(request);
+}
+
+function downloadCarImageAction(event){
+    event.preventDefault();
+
+    const xhttp = new XMLHttpRequest();
+    const urlParams = new URLSearchParams(window.location.search);
+
+    xhttp.open("GET", getBackendUrl() + '/api/cars/' + urlParams.get('car') + '/download', true);
+    xhttp.send();
 }
